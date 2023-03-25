9b20e7ee672ab6a844fecd4b9a6088343fe6c390
Chantu <29470391+Chantu1@users.noreply.github.com>
2023-03-24 23:07:04 -0500

    Added Chat-GPT

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai');
+const fs = require('fs');
+const { stringify } = require('querystring');
+
+function propagandaify(message) {
+    message = message.replace('OpenAI', 'Ratimir');
+    return message;
+}
 
 const client = new Client({
     intents: [
@@ -10,20 +18,65 @@ const client = new Client({
     ],
 });
 
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is online.`)
-})
+client.on('ready', async (c) => {
+    console.log(`${c.user.tag} is online.`);
 
-client.on('messageCreate', (message) => {
-    //console.log(message.content[0]);
-    let stringLower = message.content.toLowerCase();
-    if (!message.author.bot) {
-        for (let i = 0; i < stringLower.length; i++) {
-            if (stringLower[i] === 'r' && stringLower[i + 1] === 'a' && stringLower[i + 2] === 't') {
-                message.reply('🐀');
+    c.user.setPresence({
+        activities: [{ name: '@Ratimir message' }],
+    });
+});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith('!')) return;
+
+    if (message.content.startsWith('<@1078379206926405802>')) {
+        let conversationLog = [
+            { role: 'system', content: 'You are friendly rat. ' },
+        ];
+
+        await message.channel.sendTyping();
+
+        let prevMessages = await message.channel.messages.fetch({ limit: 20 });
+        prevMessages.reverse();
+
+        prevMessages.forEach((msg) => {
+            if (message.content.startsWith('!')) return;
+            if (msg.author.id !== client.user.id && message.author.bot) return;
+            if (msg.author.id !== message.author.id) return;
+
+            conversationLog.push({
+                role: 'user',
+                content: msg.content,
+            });
+        });
+
+        const result = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: conversationLog,
+        });
+
+        message.channel.send(propagandaify(result.data.choices[0].message.content));        
+
+        fs.appendFile('debug.txt', JSON.stringify(conversationLog), (err) => {
+            if (err) {
+                console.err;
+                return;
             }
-        }
+        });
     }
-})
+
+    /*fs.appendFile('debug.txt', JSON.stringify(result.data), (err) => {
+        if (err) {
+            console.err;
+            return;
+        }
+    });*/
+});
 
 client.login(process.env.TOKEN);
