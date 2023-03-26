29fb8a985715628bb4aa6207bdb412bca5f1b4a2
Chantu <29470391+Chantu1@users.noreply.github.com>
2023-03-26 00:25:26 -0500

    Added Chat-GPT API

---
+++ index.js
@@ -0,0 +1,117 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai');
+const { token } = require('./config.json');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command!',
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true,
+            });
+        }
+    }
+});
+
+client.once('ready', async (c) => {
+    console.log(`${c.user.tag} is online.`);
+
+    c.user.setPresence({
+        activities: [{ name: '/ratimir' }],
+    });
+});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith('!')) return;
+
+    if (message.content.startsWith('<@1078379206926405802>')) {
+        let conversationLog = [
+            { role: 'system', content: 'You are a rat named Ratimir.' },
+        ];
+
+        await message.channel.sendTyping();
+
+        let prevMessages = await message.channel.messages.fetch({
+            limit: 15,
+        });
+        prevMessages.reverse();
+
+        prevMessages.forEach((msg) => {
+            if (message.content.startsWith('!')) return;
+            if (msg.author.id !== client.user.id && message.author.bot) return;
+            if (msg.author.id !== message.author.id) return;
+
+            conversationLog.push({
+                role: 'user',
+                content: msg.content,
+            });
+        });
+        try {
+            const result = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: conversationLog,
+            });
+            message.channel.send(result.data.choices[0].message.content);
+        } catch (error) {
+            console.log(error);
+            message.channel.send(
+                'Ratimir is experiencing some issues right now. Try again later.'
+            );
+        }
+    }
+});
+
+client.login(token);
