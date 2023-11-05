ab8700d4d008a1d4c29c3626931f5bed0f7b409f
MrChantu <29470391+MrChantu@users.noreply.github.com>
2023-11-04 21:44:11 -0500

    Changed stuff

--- index.js
+++ index.js
@@ -60,30 +60,30 @@ function checkMessage(message, array = []) {
     return array;
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
+//     const command = client.commands.get(interaction.commandName);
 
-    if (!command) return;
+//     if (!command) return;
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        } else {
-            await interaction.reply({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        }
-    }
-});
+//     try {
+//         await command.execute(interaction);
+//     } catch (error) {
+//         console.error(error);
+//         if (interaction.replied || interaction.deferred) {
+//             await interaction.followUp({
+//                 content: "There was an error while executing this command!",
+//                 ephemeral: true,
+//             });
+//         } else {
+//             await interaction.reply({
+//                 content: "There was an error while executing this command!",
+//                 ephemeral: true,
+//             });
+//         }
+//     }
+// });
 
 client.once("ready", async (c) => {
     console.log(`${c.user.tag} is online.`);
