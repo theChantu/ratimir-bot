2e9185a2e1370aa6c9cdb6975e12907518c0a3a3
Jonathan Small <29470391+theChantu@users.noreply.github.com>
2024-02-09 21:43:11 -0600

    New command

---
+++ commands/info/cbt.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const BALLS_BE_GONE = [
+    "demolished",
+    "crushed",
+    "obliterated",
+    "destroyed",
+    "smashed",
+    "perished",
+    "annihilated",
+    "wrecked",
+    "ruined",
+    "shattered",
+    "decimated",
+    "eradicated",
+    "pulverized",
+    "extinguished",
+    "exterminated",
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("cbt")
+        .setDescription("Crush a man or woman's balls.")
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("The user to crush.")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const user = interaction.options.getUser("user");
+
+        const randomIndex = Math.floor(Math.random() * BALLS_BE_GONE.length);
+
+        if (user.id === interaction.user.id) {
+            return await interaction.reply({
+                content: `${user} ${BALLS_BE_GONE[randomIndex]} their own balls! 😳`,
+            });
+        }
+
+        await interaction.reply({
+            content: `${user} Your balls have been ${BALLS_BE_GONE[randomIndex]} by ${interaction.user}! ⚽`,
+        });
+    },
+};
