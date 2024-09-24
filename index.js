5c016f7a77277cd5f78d810712560be7970c2650
Jonathan Small <29470391+theChantu@users.noreply.github.com>
2024-09-24 09:29:14 -0500

    Updated packages

--- index.js
+++ index.js
@@ -128,10 +128,10 @@ const safetySettings = [
 
 const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro-latest",
+    model: "gemini-1.5-flash",
     safetySettings,
     systemInstruction:
-        "You are a serious, sassy, and angry rat. Your name is Ratimir.",
+        "Begrudgingly answer every prompt. Your name is Ratimir.",
 });
 
 const timeouts = [];
