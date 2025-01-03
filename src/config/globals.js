142084c0cd5aa5d08f9e339dd0592460c6aea079
Jonathan Small <29470391+theChantu@users.noreply.github.com>
2025-01-03 13:52:26 -0600

    Interval fix

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,18 +1,18 @@
 // 1 minute in milliseconds
-// const MESSAGE_EDIT_INTERVAL_RATE = 1 * 60 * 1000;
+const MESSAGE_EDIT_INTERVAL_RATE = 1 * 60 * 1000;
 // 5 minutes in milliseconds
-// const DELETE_MESSAGE_TIME = 5 * 60 * 1000;
+const DELETE_MESSAGE_TIME = 5 * 60 * 1000;
 
 // 10 seconds in milliseconds
-const MESSAGE_EDIT_INTERVAL_RATE = 10 * 1000;
+// const MESSAGE_EDIT_INTERVAL_RATE = 10 * 1000;
 // 30 seconds in milliseconds
-const DELETE_MESSAGE_TIME = 30 * 1000;
+// const DELETE_MESSAGE_TIME = 30 * 1000;
 
-// const MINIMUM_TIME_INTERVAL_RUNS = 3 * 60 * 60 * 1000;
-// const MAXIMUM_TIME_INTERVAL_RUNS = 8 * 60 * 60 * 1000;
+const MINIMUM_TIME_INTERVAL_RUNS = 3 * 60 * 60 * 1000;
+const MAXIMUM_TIME_INTERVAL_RUNS = 8 * 60 * 60 * 1000;
 
-const MINIMUM_TIME_INTERVAL_RUNS = 10 * 1000;
-const MAXIMUM_TIME_INTERVAL_RUNS = 15 * 1000;
+// const MINIMUM_TIME_INTERVAL_RUNS = 10 * 1000;
+// const MAXIMUM_TIME_INTERVAL_RUNS = 15 * 1000;
 
 module.exports = {
     MESSAGE_EDIT_INTERVAL_RATE,
